1184165e76c30661466ae85546e78c24794c845c
Damjan Radev <radevdamjan@gmail.com>
2023-01-13 17:05:27 +0700

    Header is now mobile responsive

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,9 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="flex items-center justify-center bg-indigo-500 text-white p-4">
+    <header className="fixed w-full flex items-center justify-center bg-indigo-500 text-white p-3 ">
       <button
-        className="block md:hidden"
+        className={`block md:hidden ${isOpen ? "hidden" : "block"}`}
         onClick={() => setIsOpen(!isOpen)}
       >
         <svg
@@ -17,15 +17,16 @@ const Header = () => {
         </svg>
       </button>
       <nav
-        className={`flex flex-col space-y-2 md:flex-row md:space-x-4 ${
-          isOpen ? "block" : "hidden md:block"
-        }`}
+        className={`flex items-center flex-col md:flex-row  ${
+          isOpen ? "flex" : "hidden md:flex"
+          }`}
+          onClick={() => {if (isOpen) setIsOpen(false)}}
       >
         <a href="#about" className="text-lg font-medium text-white hover:text-gray-300 md:block md:px-2 md:py-1">About Me</a>
         <a href="#projects" className="text-lg font-medium text-white hover:text-gray-300 md:block md:px-2 md:py-1">My Projects</a>
         <a href="#contact" className="text-lg font-medium text-white hover:text-gray-300 md:block md:px-2 md:py-1">Contact Me</a>
       </nav>
-      <div className={`fixed inset-0 bg-gray-900 opacity-75 ${isOpen ? 'block' : 'hidden'}`} onClick={() => setIsOpen(!isOpen)}></div>
+      <div className={`fixed inset-0 bg-gray-900 opacity-75 top-[108px] ${isOpen ? 'block' : 'hidden'}`} onClick={() => setIsOpen(!isOpen)}></div>
     </header>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const HomePage = () => {
   return (
     <main className="p-8">
      
-      <section id="about" className="mb-12">
+      <section id="about" className="mb-12 mt-10">
         <h2 className="text-2xl font-medium mb-4">About Me</h2>
         <p className="text-sm leading-loose">
           Meet Erik Vacev, an amateur photographer with a passion for capturing the beauty of nature and landscapes. His love for photography began at a young age and has grown over the years, as he traveled to different places and captured breathtaking images. Erik&apos;s attention to detail and his ability to capture the essence of a place make his photographs stand out. He mostly takes pictures of landscapes and nature, but occasionally he does portraits. He believes that every picture tells a story and he wants to share his stories with the world. He is always looking for new and exciting places to photograph, and he hopes to inspire others to see the world in a new way through his work.
